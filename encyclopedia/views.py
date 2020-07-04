ee16729dcbf53f131279b65927cb06f9e3152c99
Finlay <rossf.seg@gmail.com>
2020-07-04 15:07:02 -0500

    search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,19 +1,50 @@
+import markdown
+
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from . import util
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search": SearchForm()
     })
 
 def entry(request, title):
     if util.get_entry(title):
         return render(request, "encyclopedia/entry.html", {
-            "entry": util.get_entry(title)
+            "entry": markdown.markdown(util.get_entry(title)),
+            "search": SearchForm()
+
         })
     else:
         return render(request, "encyclopedia/error.html", {
-            "error": "No Entry Found."
+            "error": "No Entry Found.",
+            "search": SearchForm()
+        })
+
+def search(request):
+    results = []
+    form = SearchForm(request.POST)
+    if form.is_valid():
+        search = form.cleaned_data["search"]
+        if util.get_entry(search):
+            return HttpResponseRedirect(f"entry/{search}")
+        for i in util.list_entries():
+            if search.upper() in i.upper():
+                results.append(i)
+        return render(request, "encyclopedia/search.html", {
+            "entries": results,
+            "search": SearchForm()
+        })
+
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Invalid Search.",
+            "search": SearchForm()
         })
