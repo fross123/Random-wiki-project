e5e2dcd392fdf7ed3b5fb0f5ec0e064f32120611
Finlay <rossf.seg@gmail.com>
2020-07-05 13:21:41 -0500

    update/add comments

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -24,6 +24,7 @@ def index(request):
     })
 
 def entry(request, title):
+    # if entry matches title
     if util.get_entry(title):
         return render(request, "encyclopedia/entry.html", {
             "content": markdown2.markdown(util.get_entry(title)),
@@ -31,6 +32,7 @@ def entry(request, title):
             "title": title
 
         })
+    # entry does not exist
     else:
         return render(request, "encyclopedia/error.html", {
             "error": "No Entry Found.",
@@ -38,22 +40,22 @@ def entry(request, title):
         })
 
 def search(request):
-    # init empty list of results
+    # empty list of results
     results = []
     form = SearchForm(request.POST)
     if form.is_valid():
         search = form.cleaned_data["search"]
 
-        # if search matches entry, redirect to entry url
+        # if search matches entry, redirect to entry
         if util.get_entry(search):
             return HttpResponseRedirect(f"wiki/{search}")
 
-        # otherwise create list of matching results with links
+        # append matching results to results list
         for i in util.list_entries():
             if search.upper() in i.upper():
                 results.append(i)
 
-        # if no results in search
+        # no results from search entry
         if not len(results):
             return render(request, "encyclopedia/error.html", {
                 "error": "No results for search.",
@@ -66,7 +68,6 @@ def search(request):
                 "entries": results,
                 "search": SearchForm()
             })
-
 
     # search is invalid
     else:
@@ -82,17 +83,19 @@ def new(request):
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
 
-            # if entry has been made, render error page
+            # entry exisits, render error page
             if util.get_entry(title):
                 return render(request, "encyclopedia/error.html", {
                     "error": "This wiki already exists, please try another.",
                     "search": SearchForm()
                 })
 
-            # if title does not match current entry, save entry and redirect to new entry
+            # title does not match current entry, save entry and redirect to the new entry
             elif not util.get_entry(title):
                 util.save_entry(title, content)
                 return HttpResponseRedirect(f"wiki/{title}")
+
+        # form submission not valid
         else:
             return render(request, "encyclopedia/error.html", {
                 "error": "Invalid Submission.",
@@ -107,7 +110,9 @@ def new(request):
 
 def edit(request, title):
     if request.method == "GET":
+        # entry exisits
         if util.get_entry(title):
+            # populate form with current content
             form = EditForm(initial={"content": util.get_entry(title)})
 
             return render(request, "encyclopedia/edit.html", {
@@ -115,21 +120,27 @@ def edit(request, title):
                 "edit": form,
                 "title": title
             })
+        # enry does not exist
         else:
             return render(request, "encyclopedia/error.html", {
-                "error": "No Entry Found.",
+                "error": "No Entry Found. Please create a new one.",
                 "search": SearchForm()
             })
     elif request.method == "POST":
         form = EditForm(request.POST)
         if form.is_valid():
             content = form.cleaned_data["content"]
+
+            # save the updated entry, redirect to entry
             util.save_entry(title, content)
             return HttpResponseRedirect(f"/wiki/{title}")
 
 def random(request):
-
+    # pick a random choice from list
     x = choices(util.list_entries())
-    y = "".join(x)
 
+    # list to a str
+    y = "".join(x)
+    
+    # redirect to wiki page
     return HttpResponseRedirect(f"/wiki/{y}")
